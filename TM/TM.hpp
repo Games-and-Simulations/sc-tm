365784abfc13048bb34398be29b62bc139694676
Adakite Systems <adakitesystems@gmail.com>
2018-02-15 17:43:05 -0600

    Add BWAPI 3.7.5 support

--- TM/TM.hpp
+++ TM/TM.hpp
@@ -2,11 +2,30 @@
 
 #include "BWAPI.h"
 
+#ifdef BWAPI4
 #include <chrono>
+#elif BWAPI3
+#include <boost/chrono.hpp>
+#endif
 #include <fstream>
 
+#ifdef BWAPI4
+using SteadyClock = std::chrono::steady_clock;
+using TimePoint = std::chrono::time_point<SteadyClock>;
+using Duration = std::chrono::duration<float, std::milli>;
+#elif BWAPI3
+typedef boost::chrono::steady_clock SteadyClock;
+typedef boost::chrono::time_point<SteadyClock> TimePoint;
+typedef boost::chrono::duration<float, boost::milli> Duration;
+#endif
+
+#ifdef BWAPI4
 enum struct WinReason { Eliminated, Crash };
 enum struct Winner { Self, Enemy };
+#elif BWAPI3
+typedef enum { Eliminated, Crash } WinReason;
+typedef enum { Self, Enemy } Winner;
+#endif
 
 struct TournamentModuleManager;
 extern TournamentModuleManager tm;
@@ -19,7 +38,7 @@ BWAPI::Game *BWAPI::Broodwar;
 #include "Windows.h"
 // Assumes no unicode, ASCII only
 inline std::string envvar(const char *var) {
-  constexpr unsigned long bufSize = (1 << 15) - 1;
+  const unsigned long bufSize = (1 << 15) - 1;
   char buf[bufSize];
   const unsigned varSize = GetEnvironmentVariableA(var, buf, bufSize);
   return std::string(buf);
@@ -45,8 +64,26 @@ inline void printVar(std::ofstream &os, const char *name, const T &var,
 }
 
 struct TournamentModuleManager {
+#ifdef BWAPI4
   TournamentModuleManager() = default;
+#elif BWAPI3
+	TournamentModuleManager() {
+	  wr = Crash;
+	  winner = Enemy;
+	  building_score = 0;
+	  kill_score = 0;
+	  razing_score = 0;
+	  unit_score = 0;
+	  lastFrameTimePoint = SteadyClock::now();
+	  maxFrameTime = 0.0f;
+	  frameTimeSum = 0.0f;
+	}
+#endif
+#ifdef BWAPI4
   ~TournamentModuleManager() noexcept {
+#elif BWAPI3
+	~TournamentModuleManager() {
+#endif
     try {
       writeResults();
     } catch (...) {
@@ -54,7 +91,7 @@ struct TournamentModuleManager {
   }
   void onReset() {
     TournamentModuleManager::TournamentModuleManager();
-    frametimes.open(envvar("TM_LOG_FRAMETIMES"));
+    frametimes.open(envvar("TM_LOG_FRAMETIMES").c_str());
     frametimes << "frame_count, frame_time_max, frame_time_avg\n";
   }
 
@@ -85,11 +122,16 @@ struct TournamentModuleManager {
   }
 
   void writeResults() {
-    std::ofstream of(envvar("TM_LOG_RESULTS"));
+    std::ofstream of(envvar("TM_LOG_RESULTS").c_str());
     if (of.is_open()) {
       of << "{\n";
+#ifdef BWAPI4
       printVar(of, "is_winner", boolname(winner == Winner::Self), ",");
       printVar(of, "is_crashed", boolname(wr == WinReason::Crash), ",");
+#elif BWAPI3
+      printVar(of, "is_winner", boolname(winner == Self), ",");
+      printVar(of, "is_crashed", boolname(wr == Crash), ",");
+#endif
       printVar(of, "building_score", building_score, ",");
       printVar(of, "kill_score", kill_score, ",");
       printVar(of, "razing_score", razing_score, ",");
@@ -99,8 +141,13 @@ struct TournamentModuleManager {
   }
 
   void onEnd(bool didWin) {
+#ifdef BWAPI4
     wr = WinReason::Eliminated;
     winner = didWin ? Winner::Self : Winner::Enemy;
+#elif BWAPI3
+    wr = Eliminated;
+    winner = didWin ? Self : Enemy;
+#endif
   }
 
   void onFrame() {
@@ -115,11 +162,8 @@ struct TournamentModuleManager {
     razing_score = self->getRazingScore();
     unit_score = self->getUnitScore();
 
-    std::chrono::time_point<std::chrono::steady_clock> now =
-        std::chrono::steady_clock::now();
-    float lastFrameDuration =
-        std::chrono::duration<float, std::milli>(now - lastFrameTimePoint)
-            .count();
+    TimePoint now = SteadyClock::now();
+    float lastFrameDuration = Duration(now - lastFrameTimePoint).count();
     lastFrameTimePoint = now;
 
     if (lastFrameDuration > maxFrameTime)
@@ -133,6 +177,7 @@ struct TournamentModuleManager {
     }
   }
 
+#ifdef BWAPI4
   WinReason wr = WinReason::Crash;
   Winner winner = Winner::Enemy;
   int building_score = 0;
@@ -140,29 +185,40 @@ struct TournamentModuleManager {
   int razing_score = 0;
   int unit_score = 0;
   std::ofstream frametimes;
-  std::chrono::time_point<std::chrono::steady_clock> lastFrameTimePoint =
-      std::chrono::steady_clock::now();
+  std::chrono::time_point<SteadyClock> lastFrameTimePoint = SteadyClock::now();
   float maxFrameTime = 0.0f;
   float frameTimeSum = 0.0f;
+#elif BWAPI3
+  WinReason wr;
+  Winner winner;
+  int building_score;
+  int kill_score;
+  int razing_score;
+  int unit_score;
+  std::ofstream frametimes;
+  TimePoint lastFrameTimePoint;
+  float maxFrameTime;
+  float frameTimeSum;
+#endif
 
   struct TournamentModule : public BWAPI::TournamentModule {
 #ifdef BWAPI4
-    inline virtual bool onAction(BWAPI::Tournament::ActionID action,
+    virtual bool onAction(BWAPI::Tournament::ActionID action,
 #elif BWAPI3
-    inline virtual bool onAction(int action,
+    virtual bool onAction(int action,
 #endif
                                  void *parameter) override {
       return tm.onAction(action, parameter);
     }
-    inline virtual void onFirstAdvertisement() override {}
+    virtual void onFirstAdvertisement() override {}
   };
 
   TournamentModule *mod;
 
   struct TournamentBot : public BWAPI::AIModule {
-    inline virtual void onStart() override { tm.onReset(); }
-    inline virtual void onFrame() override { tm.onFrame(); }
-    inline virtual void onEnd(bool didWin) override { tm.onEnd(didWin); }
+    virtual void onStart() override { tm.onReset(); }
+    virtual void onFrame() override { tm.onFrame(); }
+    virtual void onEnd(bool didWin) override { tm.onEnd(didWin); }
   };
 
   TournamentBot *bot;
